7c06eccc727b9e1152570ccb80d18e2496189ec9
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-05-30 13:44:19 -0300

    leitura de argumentos passados pela linha de comando

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+
+pub struct Config {
+    pub tab_width: u32,
 }
+
+fn main() {
+    let args_iter = env::args();
+    
+    let filename = match args_iter.skip(1).next(){
+        Some(arg) => arg,
+        None => {
+            eprintln!("Insufficient arguments: missing the filename");
+            process::exit(1);
+        }
+    };
+    println!("{}", filename)
+}
+
