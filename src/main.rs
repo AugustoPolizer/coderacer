3d4f7d583dc1248baa0284583500beb037205e74
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-05-30 15:22:15 -0300

    Arquivo de configuração

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::env;
+use std::fs;
 use std::process;
+use serde::{Deserialize, Serialize};
 
-pub struct Config {
-    pub tab_width: u32,
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    tab_width: u32,
+}
+
+impl Config {
+    fn new() -> Config {
+        Config { tab_width: 4}
+    }
 }
 
 fn main() {
@@ -15,6 +24,25 @@ fn main() {
             process::exit(1);
         }
     };
-    println!("{}", filename)
+
+    let config = match fs::read_to_string(".coderacer.json") {
+        Ok(config_str) => {
+            let parsed_config : Config = match serde_json::from_str(&config_str) {
+                    Ok(x) => x,
+                    Err(e) => {
+                        eprintln!("Parsing error on config file: {}", e);
+                        Config::new()
+                    }
+            };
+            parsed_config
+        },
+        Err(_) => {
+            Config::new()
+        }
+    };
+
+    println!("{} {:?}", filename, config);
+
+    println!("{:?}", std::env::current_exe());
 }
 
