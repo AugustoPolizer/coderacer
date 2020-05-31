6abb676b2fd416114a4f2bff1f669c2d913133be
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-05-30 22:05:58 -0300

    implementação do buffer de output e do raw mode

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-
 use serde::{Deserialize, Serialize};
-
+use termion::raw::IntoRawMode;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
@@ -13,13 +12,19 @@ impl Config {
     }
 }
 
-pub fn run(filename: &str, _config: &Config) -> Result<(), std::io::Error>{
+pub fn run(filename: &str, config: &Config) -> Result<(), std::io::Error>{
     let file_content = file_preprocessing::read_file(filename)?;
     let buffer = file_preprocessing::parse_file_to_buffer(&file_content);
-
-    text_viewer::write_buffer_to_screen(& buffer)?;
-
+    
+    let mut stdout = std::io::stdout().into_raw_mode()?;
+    print!("{} {}", termion::clear::All, termion::cursor::Goto(1,1));
+    text_viewer::write_buffer_to_screen(& mut stdout, & buffer, config.tab_width)?;
+    
     Ok(())
+}
+
+pub fn input_layer() {
+    
 }
 
 pub mod file_preprocessing{
@@ -41,17 +46,28 @@ pub mod file_preprocessing{
 }
 
 pub mod text_viewer {
-    extern crate termion;
-    use std::io::Write;
+    use std::io::{self, Write};
 
-    pub fn write_buffer_to_screen(buffer: & Vec<& str>) -> Result<(), std::io::Error>{
-        let stdout = std::io::stdout();
-        let mut handle = stdout.lock();
-        print!("{} {}", termion::clear::All, termion::cursor::Goto(1,1));
-        for lines in buffer {
-            handle.write(lines.as_bytes())?;
-            handle.write("\n".as_bytes())?;
+    pub fn write_buffer_to_screen(stdout : & mut io::Stdout,text_buffer: & Vec<& str>, tab_width: u32) -> Result<(), std::io::Error>{
+        let mut buffer_output = String::new(); 
+        for lines in text_buffer {
+            for c in lines.chars() {
+                if c == '\t' {
+                    let mut spaces = 0;
+                    while spaces < tab_width {
+                        buffer_output.push(' ');
+                        spaces += 1;
+                    }
+                }
+                else {
+                    buffer_output.push(c);
+                }
+            }
+            buffer_output.push('\n');
+            buffer_output.push('\r');
         } 
+
+        write!(stdout, "{}", buffer_output)?;
         Ok(())
     }
 }
