f667badccbf56b01c6552a2686ec7e6acbd73a9f
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-05-31 16:17:34 -0300

    Diversas refatorações e input layer basica implementada

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
-use termion::raw::IntoRawMode;
+use termion::{raw::IntoRawMode, event::Key, input::TermRead};
+use std::io::{self, stdout, stdin};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
-    tab_width: u32,
+    tab_width: u8,
 }
 
 impl Config {
@@ -12,59 +13,91 @@ impl Config {
     }
 }
 
+struct Screen {
+    rows: u16,
+    cols: u16,
+    cursor:(u16, u16)
+}
+
+impl Screen {
+    pub fn new() -> Result<Screen, io::Error> {
+        let size = termion::terminal_size()?;
+        Ok(Screen {
+            cols: size.0,
+            rows: size.1,
+            cursor: (1,1)
+        })
+    }
+}
+
+
+
+
 pub fn run(filename: &str, config: &Config) -> Result<(), std::io::Error>{
-    let file_content = file_preprocessing::read_file(filename)?;
-    let buffer = file_preprocessing::parse_file_to_buffer(&file_content);
-    
-    let mut stdout = std::io::stdout().into_raw_mode()?;
+    // Initializations  
+    let buffer = file_processing::read_file_to_buffer(filename, config.tab_width)?;
+    let screen = Screen::new()?;
+    let mut stdout = stdout().into_raw_mode()?;
+
     print!("{} {}", termion::clear::All, termion::cursor::Goto(1,1));
-    text_viewer::write_buffer_to_screen(& mut stdout, & buffer, config.tab_width)?;
-    
+    text_viewer::refresh_screen(& mut stdout, &buffer, screen.rows, 0)?;
+
+    let stdin = stdin(); 
+    input_layer(stdin);
     Ok(())
 }
 
-pub fn input_layer() {
-    
+
+
+fn input_layer(stdin: io::Stdin) {
+  
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Ctrl('q') => break,
+            Key::Char(c)   => println!("{}", c),
+            _              => println!("Other"),
+        } 
+    }  
 }
 
-pub mod file_preprocessing{
+mod file_processing {
     use std::fs;
 
-    pub fn read_file(filename: &str) -> Result<String, std::io::Error >{
+    pub fn read_file_to_buffer(filename: &str, tab_width: u8) -> Result<Vec<String>, std::io::Error >{
         let file_content = fs::read_to_string(filename)?;
-        Ok(file_content) 
-    }
-
-    pub fn parse_file_to_buffer<'a>(file_content: &'a str) -> Vec<&'a str>{
+        
         let mut buffer = Vec::new();
-
-        for line in file_content.lines() {
-            buffer.push(line);
-        }
-        buffer
-    }
-}
-
-pub mod text_viewer {
-    use std::io::{self, Write};
-
-    pub fn write_buffer_to_screen(stdout : & mut io::Stdout,text_buffer: & Vec<& str>, tab_width: u32) -> Result<(), std::io::Error>{
-        let mut buffer_output = String::new(); 
-        for lines in text_buffer {
+        for lines in file_content.lines(){
+            let mut line = String::new();
             for c in lines.chars() {
                 if c == '\t' {
                     let mut spaces = 0;
-                    while spaces < tab_width {
-                        buffer_output.push(' ');
+                    while spaces < tab_width && spaces < 8{
+                        line.push(' ');
                         spaces += 1;
                     }
-                }
-                else {
-                    buffer_output.push(c);
+                } else {
+                    line.push(c);
                 }
             }
-            buffer_output.push('\n');
-            buffer_output.push('\r');
+            buffer.push(line);
+        }
+        Ok(buffer) 
+    }
+}
+
+mod text_viewer {
+    use std::io::{self, Write};
+
+    pub fn refresh_screen(stdout : & mut io::Stdout,buffer: & Vec<String>, screen_rows: u16, starting_row: u32)
+        -> Result<(), std::io::Error> {
+        let mut buffer_output = String::new(); 
+        for row_index in starting_row .. starting_row + screen_rows as u32 + 1{ 
+            if row_index as usize >= buffer.len() {
+                break;
+            }
+            buffer_output.push_str(&buffer[row_index as usize]);
+            buffer_output.push_str("\n\r");
         } 
 
         write!(stdout, "{}", buffer_output)?;
